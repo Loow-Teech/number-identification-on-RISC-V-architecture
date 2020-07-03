ed8c56b6f75723aaa2a732bede93c5bf83511bb5
Leo Levin <jzlzcaa@gmail.com>
2020-07-03 21:05:05 +0800

    Fetched new version
    Added FTP function

--- riscv64/bashrun.sh
+++ riscv64/bashrun.sh
@@ -1,6 +1,23 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
 
+ftp -i -v -n 192.168.123.109  <<EOF
+user anonymous 1
+binary
+cd /1/
+lcd ./id/
+get logoword.jpg
+bye
+EOF
+ 
+rm -rf ./result.txt
 for((i=1;i<11;i++)); do
 echo -e \\n;
 ./main ./id/$i.jpg
 done
+
+ftp -i -v -n 192.168.123.109  <<EOF
+user anonymous 1
+binary
+put result.txt
+bye
+EOF
